dec6bc0f488b955a6176540573207f7e5ab78684
afloemkis <chere.aleksandra@gmail.com>
2023-07-12 23:23:57 +0300

    build.Gradle updated

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -5,10 +5,64 @@ import org.junit.Assert.*
 class WallServiceTest {
 
     @Test
-    fun add() {
+    fun addPost_IdIsNotZero() {
+        val post = Post(
+            id = 0,
+            ownerId = 1,
+            fromId = 1,
+            createdBy = 1,
+            date = 123456789,
+            text = "Test post",
+            comments = CommentObject(0, true, true),
+            likes = LikeObject(0, false, true, true),
+            reposts = RepostObject(0, false),
+            views = ViewsObject(0)
+        )
+
+        val addedPost = WallService.add(post)
+
+        assertTrue(addedPost.id != 0)
     }
 
     @Test
-    fun update() {
+    fun updatePost_ExistingId_ReturnsTrue() {
+        val existingPost = Post(
+            id = 1,
+            ownerId = 1,
+            fromId = 1,
+            createdBy = 1,
+            date = 123456789,
+            text = "Existing post",
+            comments = CommentObject(0, true, true),
+            likes = LikeObject(0, false, true, true),
+            reposts = RepostObject(0, false),
+            views = ViewsObject(0)
+        )
+        WallService.add(existingPost)
+
+        val updatedPost = existingPost.copy(text = "Updated post")
+        val isUpdated = WallService.update(updatedPost)
+
+        assertTrue(isUpdated)
+    }
+
+    @Test
+    fun updatePost_NonexistentId_ReturnsFalse() {
+        val nonExistentPost = Post(
+            id = 2,
+            ownerId = 1,
+            fromId = 1,
+            createdBy = 1,
+            date = 123456789,
+            text = "Nonexistent post",
+            comments = CommentObject(0, true, true),
+            likes = LikeObject(0, false, true, true),
+            reposts = RepostObject(0, false),
+            views = ViewsObject(0)
+        )
+
+        val isUpdated = WallService.update(nonExistentPost)
+
+        assertFalse(isUpdated)
     }
 }
